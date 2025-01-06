279446fefe7cf499a546f8157f34dcc4cea5ee3c
Aruljo55 <aruljothinanjappan12@gmail.com>
2025-01-06 21:18:58 +0530

    power

---
+++ power.py
@@ -0,0 +1,8 @@
+# Read inputs
+a = int(input())
+b = int(input())
+m = int(input())
+
+# Calculate and print the results
+print(pow(a, b))       # First line: a^b
+print(pow(a, b, m))    # Second line: (a^b) % m
