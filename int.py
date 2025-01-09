45b25d9c83bc97f4125be8437e0ffc8e0dd94c0e
Aruljo55 <aruljothinanjappan12@gmail.com>
2025-01-09 15:12:54 +0530

    integers comes in all size

---
+++ int.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())  # Read the first integer a
+    b = int(input())  # Read the second integer b
+    c = int(input())  # Read the third integer c
+    d = int(input())  # Read the fourth integer d
+    
+    # Calculate the result of a^b + c^d
+    result = pow(a, b) + pow(c, d)
+    
+    # Print the result
+    print(result)
