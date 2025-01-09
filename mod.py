048b3f7ce6783e3632be7117e650e8a046d1cd44
Aruljo55 <aruljothinanjappan12@gmail.com>
2025-01-09 15:19:10 +0530

    mod divmod

---
+++ mod.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())  # Read the first integer
+    b = int(input())  # Read the second integer
+    
+    print(a // b)  # Print the result of integer division
+    print(a % b)   # Print the result of the modulo operation
+    print(divmod(a, b))  # Print the result of divmod function
