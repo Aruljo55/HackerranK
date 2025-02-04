c647c5e60b754fde58a0d931e68514bd4d7e6598
Aruljo55 <aruljothinanjappan12@gmail.com>
2025-02-04 19:07:42 +0530

    mean,var and standard division

---
+++ mvs.py
@@ -0,0 +1,6 @@
+import numpy
+n,m=map(int,input().split())
+A=numpy.array([[*map(int,input().split())]for i in range(n)])
+print(numpy.mean(A,axis=1))
+print(numpy.var(A,axis=0))
+print(round(numpy.std(A),11))
